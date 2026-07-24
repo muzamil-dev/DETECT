56583d7005af88960530fd149dbcddacbf93d954
Muzamil <mshamsifl@gmail.com>
2026-07-24 00:41:48 -0400

    unified single-container configuration

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,10 +4,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import preact from "@astrojs/preact";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "server",
+  adapter: node({ mode: "standalone" }),
   vite: {
     server: {
       host: true,
